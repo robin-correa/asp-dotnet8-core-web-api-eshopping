03a96c473f74e5e983180351781e09abc39b8f7e
robin.correa <robin.correa21@gmail.com>
2024-06-02 09:39:53 +0800

    Create Product controller with Index() and Show() actions.

---
+++ EShopping.API/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using EShopping.API.Data;
+using EShopping.API.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShopping.API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+        public ProductController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<Product> productsQuery = _appDbContext.Products.Include(p => p.ProductCategories);
+
+            List<Product> products = await productsQuery.ToListAsync();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Show(int id)
+        {
+            var product = await _appDbContext.Products.Include(p => p.ProductCategories).FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+    }
+}
